eee83f9eee7b6dda3a66871037d95089e3250e5e
Aaron Cayabyab <aa.cayabyab8@gmail.com>
2017-12-12 09:46:09 -0500

    Added CRX and Unity Projects

---
+++ Unity Projects/Cube Dash/Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	bool gameHasEnded = false;
+
+	public GameObject completeLevelUI;
+
+	public void CompleteLevel() {
+		completeLevelUI.SetActive (true);
+	}
+
+	public void EndGame() {
+
+		if (gameHasEnded == false) {
+			gameHasEnded = true;
+			Debug.Log ("HI");		
+
+			//Restart game
+			Invoke("Restart", 1f);
+		}
+
+	}
+
+	void Restart() {
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+	}
+
+	public void Quit() {
+		Debug.Log ("Exit Game");
+		Application.Quit ();
+	}
+
+	public void StartGame() {
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+
+	public void Replay() {
+		SceneManager.LoadScene ("Menu");
+	}
+
+
+}
---
+++ Unity Projects/Cube Dash/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+	public Rigidbody player;
+	public float travelSpeed = 2000f;
+	public float controlSpeed = 120f;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		player.AddForce (0, 0, travelSpeed * Time.deltaTime);
+
+		if (Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow)) {
+			player.AddForce (controlSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+
+		if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) {
+			player.AddForce (-controlSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+
+		if (player.position.y < -1f) {
+			FindObjectOfType<GameManager> ().EndGame();
+		}
+		
+	}
+}
