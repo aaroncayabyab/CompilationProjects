aac1135787e1ef2a823e5792952d7aba0b20d268
Aaron Cayabyab <aa.cayabyab8@gmail.com>
2017-12-12 09:51:04 -0500

    Added Unity Projects

---
+++ Unity Projects/Block Breaker/Assets/Scripts/Brick.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Brick : MonoBehaviour {
+	
+	public AudioClip crack;
+	public Sprite[] hitSprites;
+	public static int breakableCount = 0;
+	public GameObject smoke;
+
+	private int timesHit;
+	private LevelManager levelManager;
+	private bool isBreakable;
+
+	// Use this for initialization
+	void Start () {
+		isBreakable = (this.tag == "Breakable");
+		//Keep track of breakable bricks
+		if (isBreakable) {
+			breakableCount++;
+		}
+
+		timesHit = 0;
+		levelManager = GameObject.FindObjectOfType<LevelManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D col) {
+		AudioSource.PlayClipAtPoint (crack, transform.position, 0.8f);
+		if (isBreakable) {
+			HandleHits ();
+		}
+	}
+
+	void HandleHits() {
+		timesHit++;
+		int maxHits = hitSprites.Length + 1;
+		if (timesHit >= maxHits) {
+			breakableCount--;
+			levelManager.BrickDestroyed ();
+			PuffSmoke ();
+			Destroy (gameObject);
+		} else {
+			LoadSprites ();
+		}
+	}
+
+	void PuffSmoke() {
+		var smokePuff = smoke.GetComponent<ParticleSystem> ().main;
+		smokePuff.startColor = gameObject.GetComponent<SpriteRenderer> ().color;
+		Instantiate (smoke, gameObject.transform.position, Quaternion.identity);
+	
+	}
+
+	void LoadSprites() {
+		int spriteIndex = timesHit - 1;
+		if (hitSprites [spriteIndex] != null) {
+			this.GetComponent<SpriteRenderer> ().sprite = hitSprites [spriteIndex];
+		} else {
+			Debug.LogError ("Brick Sprite Missing");
+		}
+	}
+
+	//TODO remove this after game actuall winnable
+	void SimulateWin() {
+		levelManager.LoadNextLevel ();
+	}
+}
